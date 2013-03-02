d1b91308004facb7c334d8510347b7d25fcb83f4
Marc Brinkmann <git@marcbrinkmann.de>
2013-03-02 16:20:14 +0100

    Initial import.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='shutilwhich',
+    version='1.0',
+    description='shutil.which for those not using Python 3.3 yet.',
+    long_description=read('README.rst'),
+    author='Marc Brinkmann',
+    author_email='git@marcbrinkmann.de',
+    url='http://github.com/mbr/shutilwhich',
+    license='PSF',
+    py_modules=['shutilwhich'],
+)
---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import shutil
+
+import shutilwhich
+
+print shutil.which('python')
